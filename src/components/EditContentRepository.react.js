7a4b5b9a96408ba40cdbd1d4d0d3e8507e851f35
Ash Wilson <smashwilson@gmail.com>
2015-08-10 13:42:50 -0400

    Style the EditContent component.

--- src/components/EditContentRepository.react.js
+++ src/components/EditContentRepository.react.js
@@ -25,22 +25,24 @@ var EditContentRepository = React.createClass({
     return (
       <div className="edit-content-repository">
         <Header />
-        <h1>New Content Repository</h1>
-        <div className="repository-path">
-          <h3>Repository Path</h3>
-          <p className="explanation">Filesystem path to the content repository.</p>
-          <input id="input-repository-path" type="text" className="line" placeholder="/some/path" onChange={this.handleRepositoryPathChange}></input>
-        </div>
-        <div className="control-repository">
-          <h3>Control Repository Location</h3>
-          <p className="explanation">
-            Location of the control repository. May be either a git URL or a local filesystem path.
-          </p>
-          <input id="control-repository" type="text" className="line" placeholder="https://github.com/deconst/deconst-docs-control.git" onChange={this.handleControlRepositoryChange}></input>
-        </div>
-        <div className="controls">
-          <button className="btn btn-large btn-default" onClick={this.handleCancel}>Cancel</button>
-          <button className="btn btn-large btn-primary" onClick={this.handleCreate}>Create</button>
+        <div className="container">
+          <h1>New Content Repository</h1>
+          <div className="repository-path">
+            <h3>Repository Path</h3>
+            <p className="explanation">Filesystem path to the content repository.</p>
+            <input id="input-repository-path" type="text" className="line" placeholder="/some/path" onChange={this.handleRepositoryPathChange}></input>
+          </div>
+          <div className="control-repository">
+            <h3>Control Repository Location</h3>
+            <p className="explanation">
+              Location of the control repository. May be either a git URL or a local filesystem path.
+            </p>
+            <input id="control-repository" type="text" className="line" placeholder="https://github.com/deconst/deconst-docs-control.git" onChange={this.handleControlRepositoryChange}></input>
+          </div>
+          <div className="controls">
+            <button className="btn btn-large btn-default" onClick={this.handleCancel}>Cancel</button>
+            <button className="btn btn-large btn-primary" onClick={this.handleCreate}>Create</button>
+          </div>
         </div>
       </div>
     )
