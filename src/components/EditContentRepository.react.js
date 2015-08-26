9a3ba4e7a8d1bc5ee9d015419fca1ac25785cb7f
Ash Wilson <smashwilson@gmail.com>
2015-08-26 14:25:25 -0400

    Native dialog controls.

--- src/components/EditContentRepository.react.js
+++ src/components/EditContentRepository.react.js
@@ -1,5 +1,9 @@
 import React from 'react/addons';
 import Router from 'react-router';
+import remote from 'remote';
+
+var dialog = remote.require('dialog');
+
 import {ContentRepository} from '../utils/ContentRepositoryUtil';
 import ContentRepositoryActions from '../actions/ContentRepositoryActions';
 import ContentRepositoryStore from '../stores/ContentRepositoryStore';
@@ -10,8 +14,8 @@ var EditContentRepository = React.createClass({
   getInitialState: function () {
     return {
       isNew: true,
-      contentRepositoryPath: "/Users/ashl6947/writing/docs-quickstart",
-      controlRepositoryLocation: "/Users/ashl6947/writing/nexus-control",
+      contentRepositoryPath: null,
+      controlRepositoryLocation: null,
       preparer: "sphinx"
     };
   },
@@ -28,6 +32,28 @@ var EditContentRepository = React.createClass({
         controlRepositoryLocation: repo.controlRepositoryLocation,
         preparer: repo.preparer
       });
+    }
+  },
+
+  handleOpenContent: function() {
+    let results = dialog.showOpenDialog({
+      title: 'Content Repository',
+      properties: ['openDirectory']
+    });
+
+    if (results && results.length > 0) {
+      this.setState({contentRepositoryPath: results[0]});
+    }
+  },
+
+  handleOpenControl: function () {
+    let results = dialog.showOpenDialog({
+      title: 'Control Repository',
+      properties: ['openDirectory']
+    });
+
+    if (results && results.length > 0) {
+      this.setState({controlRepositoryLocation: results[0]});
     }
   },
 
@@ -80,6 +106,7 @@ var EditContentRepository = React.createClass({
             <h3>Content Repository Path</h3>
             <p className="explanation">Filesystem path to the content repository.</p>
             <input type="text" className="line" value={this.state.contentRepositoryPath} placeholder="/some/path" onChange={this.handleRepositoryPathChange}></input>
+            <button className="btn btn-default btn-sm browse" onClick={this.handleOpenContent}>browse</button>
           </div>
           <div className="control-repository">
             <h3>Control Repository Location</h3>
@@ -87,6 +114,7 @@ var EditContentRepository = React.createClass({
               Location of the control repository. May be either a git URL or a local filesystem path.
             </p>
             <input type="text" className="line" value={this.state.controlRepositoryLocation} placeholder="https://github.com/deconst/deconst-docs-control.git" onChange={this.handleControlRepositoryChange}></input>
+            <button className="btn btn-default btn-sm browse" onClick={this.handleOpenControl}>browse</button>
           </div>
           <div className="preparer">
             <h3>Preparer</h3>
