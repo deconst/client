aca28b821bcc879df9cec4b40a580ca526aed937
Ash Wilson <smashwilson@gmail.com>
2015-08-31 08:54:04 -0400

    Ignore changes to `node_modules` everywhere.

--- src/stores/ContentRepositoryStore.js
+++ src/stores/ContentRepositoryStore.js
@@ -56,7 +56,8 @@ class ContentRepositoryStore {
     ContentRepositoryUtil.launchControlPreparer(r);
 
     let installWatcher = (root, fn, callback) => {
-      let ignored = ['_build/**', '_site/**', '.git/**', '.DS_Store', 'npm-debug.log', 'build'];
+      let ignored = ['_build/**', '_site/**', '.git/**', '.DS_Store', 'npm-debug.log', 'build',
+        '**/node_modules/**'];
       let gitignorePath = path.join(root, ".gitignore");
 
       fs.readFile(gitignorePath, {encoding: 'utf-8'}, (error, content) => {
